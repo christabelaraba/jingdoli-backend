0be82d98f8c538edde15e08df8aaea9d514fb081
kkkofi <kofiababio07@gmail.com>
2024-06-30 00:17:15 +0000

    list of producct

---
+++ src/models/product.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define('Product', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        model: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        frequency: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        fuel_type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        prime: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        createdAt: {
+            field: 'created_at',
+            type: DataTypes.DATE,
+        },
+        updatedAt: {
+            field: 'updated_at',
+            type: DataTypes.DATE,
+        },
+    });
+  
+    return Product;
+};
